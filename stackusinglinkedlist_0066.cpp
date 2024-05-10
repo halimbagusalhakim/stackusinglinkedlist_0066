95ce7cf5f03dd46a5b865b4b58713d5d0572289d
halimbagusalhakim <halsat132@gmail.com>
2024-05-10 15:10:16 +0700

    update pointer

--- stackusinglinkedlist_0066.cpp
+++ stackusinglinkedlist_0066.cpp
@@ -22,7 +22,8 @@ class Stack {
         int push(int value) {
             Node* newNode = new Node();
             newNode->data = value;
-            
+            newNode->next = top;
+            top = newNode;
 
         }
 };
