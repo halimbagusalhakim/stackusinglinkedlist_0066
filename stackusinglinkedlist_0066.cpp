f4035f1992a4fe63a4c04b5f7bcb042a2d6e9746
halimbagusalhakim <halsat132@gmail.com>
2024-05-10 15:50:37 +0700

    selesai

--- stackusinglinkedlist_0066.cpp
+++ stackusinglinkedlist_0066.cpp
@@ -77,6 +77,33 @@ int main()
         cin >> value;
         stack.push(value);
         break;
+    case 2:
+        if (!stack.isEmpty()) {
+            stack.pop();
+        }
+        else {
+            cout << "Stack is empty.cannot pop" << endl;
+        }
+        break;
+    case 3:
+        if (!stack.isEmpty()) {
+            stack.peek();
+        }
+        else {
+            cout << " Stack is empty. No top value." << endl;
+
+        }
+        break;
+    case 4:
+        cout << " Exiting program " << endl;
+        break;
+    default:
+        cout << "invalid choice. try again" << endl;
+        break;
+
     }
+    cout << endl;
+    return 0;
+
 }
 
